6d728e5698f2a3fa07eba4eeb640213be2e4b6f2
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-23 21:21:38 -0700

    more modifications, now sending the filename and file contents back to client.

--- client.js
+++ client.js
@@ -1,7 +1,14 @@
 const net = require("net");
 const port = 8009;
 const fs = require('fs');
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let fileName = "";
 const connectionConfig = { // doing it this way provides some self documentation...
   port: port,
   host: 'localhost' // dns server will return 127.0.0.1 (same computer you're on now) when you're ready to split client and server, then you can update to a host on a server elsewhere.
@@ -15,55 +22,61 @@ client.on('connect', () => {
   console.log(`client is connected to the fileserver`);
 });
 
-process.stdin.on('data', fileToSearchFor => {
-  process.stdin.setEncoding('utf8');
-  client.write(fileToSearchFor.toString());
-})
+rl.question(`What file are you looking for?\n`, (input) => {
+  client.write(input);
+  fileName = input;
+  rl.close();
+});
 
-client.on('data', message => {
-  console.log('server sent:', message);
+client.on('data', data => {
+  // request(URL, (error, response, message) => {
+  //   // edge case 3: url is invalid
+  //   if (error) {
+  //     if (error.code === 'ENOTFOUND') {
+  //       console.log(`${URL} is not a valid URL.`);
+  //       process.exit();
+  //     } else {
+  //       console.log(error);
+  //     }
+  const arrayOfStrings = data.split(":");
+  const fileName = arrayOfStrings[0];
+  const bodyOfFile = arrayOfStrings[1]
+  console.log(fileName);
+  console.log(bodyOfFile); 
+  if (fs.existsSync("./"+fileName)) {   // edge case 1: file already exists
+    rl.question(`File already exists. Would you like to override ${fileName}?\n Press 'Y' then enter to confirm or enter to exit.\n`, (input) => {
+      if (input === 'Y') {
+        writeTheFile("./"+fileName, bodyOfFile);
+      } else {
+        rl.close();
+      }
+    });
+  } else { // file doesn't already exist so okay to write
+    writeTheFile("./"+fileName, bodyOfFile);
+  }
+});
+
+  // const writeTheFile = function(fileName, body) {
+  //   fs.writeFile(fileName, body, 'utf8', (err) => {
+  //     if (err) {
+  //       // edge case 2: file path is invalid
+  //       if (err.code === 'ENOENT') {
+  //         console.log("File path is invalid.");
+  //         process.exit();
+  //       } else {
+  //         console.log(err);
+  //         process.exit();
+  //       }
+  //     } else {
+  //       fs.stat(fileName, (err, stats) => {
+  //         console.log(`Downloaded and saved ${stats.size} bytes to ${fileName}`);
+  //         process.exit();
+  //       });
+  //     }
+  //   });
+  // };
+
 });
 
 // Clients can connect to the server via TCP and send a request for a file (by filename)
 
-// request(URL, (error, response, body) => {
-//   // edge case 3: url is invalid
-//   if (error) {
-//     if (error.code === 'ENOTFOUND') {
-//       console.log(`${URL} is not a valid URL.`);
-//       process.exit();
-//     } else {
-//       console.log(error);
-//     }
-//   } else if (fs.existsSync(fileName)) {   // edge case 1: file already exists
-//     rl.question(`File already exists. Would you like to override ${fileName}?\n Press 'Y' then enter to confirm or enter to exit.\n`, (input) => {
-//       if (input === 'Y') {
-//         writeTheFile(fileName, body);
-//       } else {
-//         rl.close();
-//       }
-//     });
-//   } else { // file doesn't already exist so okay to write
-//     writeTheFile(fileName, body);
-//   }
-// });
-
-// const writeTheFile = function(fileName, body) {
-//   fs.writeFile(fileName, body, 'utf8', (err) => {
-//     if (err) {
-//       // edge case 2: file path is invalid
-//       if (err.code === 'ENOENT') {
-//         console.log("File path is invalid.");
-//         process.exit();
-//       } else {
-//         console.log(err);
-//         process.exit();
-//       }
-//     } else {
-//       fs.stat(fileName, (err, stats) => {
-//         console.log(`Downloaded and saved ${stats.size} bytes to ${fileName}`);
-//         process.exit();
-//       });
-//     }
-//   });
-// };
--- server.js
+++ server.js
@@ -9,37 +9,43 @@ const connectedClients = [];
 console.log(files);
 
 server.on('connection', (client) => {
+  let found = false;
   connectedClients.push (client);
   console.log(`Connection has been made by client.`);
   client.setEncoding('utf8');
 
   client.write('Welcome to my awesome file server! 🥰\n');
-  client.write('What file are you looking for?\n');
-  //client.write(`${files}`);
+  //client.write('What file are you looking for?\n');
 
   client.on('data', (fileToSearchFor) => {
-    fileToSearchFor.replace(/^\s\n+|\s\n+$/g,'');
-    client.write(`Searching for 123${fileToSearchFor}123`);
+    client.write(`Searching for ${fileToSearchFor}...`);
     client.write(`In directory: ${fileDir} which contains ${files}\n`);
 
     for (const file of files) {
-      console.log("does", fileToSearchFor, "===", file, "?", fileToSearchFor === file);
-      console.log(typeof(file), typeof(fileToSearchFor));
-      //client.write(file, "\n");//, "in", files, "?", "\n");
-      if (file == fileToSearchFor) {
-        console.log(`File: ${fileToSearchFor} was found!\n`);
+      //console.log("does", fileToSearchFor, "===", file, "?", fileToSearchFor === file);
+      //console.log(typeof(file), typeof(fileToSearchFor));
+      if (file === fileToSearchFor) {
+        //console.log(`File: ${fileToSearchFor} was found!\n`);
         client.write(`File: ${fileToSearchFor} was found!\n`)
-        break;
+        found = true;
+        console.log(fileDir+"/"+file);
+        fs.readFile(fileDir+"/"+file, 'utf8', function (err, file) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(`reading file...`);
+          client.write(fileToSearchFor + ":" + file);
+        });
+
       } 
     }
-    
-    client.write(`File: ${fileToSearchFor} was not found...\n`)
-    console.log('now what?\n');
+    if (!found) {
+      client.write(`Sorry, the file: ${fileToSearchFor} was not found...\n`)
+    }
+    //process.exit();
   });
 });
 
 server.listen(port, function (){
   console.log(`Server is listening on port=${port}`);
 });
-
-// The server looks for requested files locally and sends back the data
